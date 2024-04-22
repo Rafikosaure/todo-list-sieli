7b7248eda5baf0dd3d689be249d23b071869bb6f
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-04-22 17:04:52 +0200

    Début de l'implémentation de la création d'un nouvel utilisateur.

--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -1,12 +1,29 @@
+import { useState } from 'react'
 import '../App.css'
 
 export default function SignUp() {
+
+    const [ user, setUser ] = useState({})
+
+    const handleChange = (event) => {
+        const { name, value } = event.target
+        console.log({ name, value })
+        setUser((user) => ({ ...user, [name]: value }))
+        console.log(user)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log(event.target)
+    }
+
+
   return (
     <div className='main-div'>
         <h2>Inscription</h2>
-      <form className='login-form'>
-          <input type="email" id="email" name='email' placeholder='email' required />
-          <input type="password" id="password" name='password' placeholder='mot de passe' required />
+      <form className='login-form' onSubmit={handleSubmit}>
+          <input type="email" id="email" name='email' placeholder='email' required onChange={handleChange} />
+          <input type="password" id="password" name='password' placeholder='mot de passe' required onChange={handleChange} />
           <button type='submit'>S'inscrire</button>
         </form>
     </div>
