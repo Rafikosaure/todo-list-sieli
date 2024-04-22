97d995231b8df93a368943e4471890b650ac9b1f
Dorsan_Julien <juliendorsan@gmail.com>
2024-04-22 16:10:23 +0200

    Ajout de la loqique pour enregistrer une tâche en base de donnéesÃ

--- frontend/src/Pages/dashboard.jsx
+++ frontend/src/Pages/dashboard.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import axios from "axios";
+import { URL } from '../Utils/URL';
 
 function Dashboard() {
 
@@ -10,20 +12,38 @@ function Dashboard() {
         // récupération du nom et de la valeur de l'évènement
         const  { name, value} = e.target;
         // changement du state task
-        setTask((task) => ({...task, [name]:value}));
+        name == "priority" ? setTask((task) => ({...task, [name]:Number(value)})) : setTask((task) => ({...task, [name]:value}));
         console.log(task); 
+    }
+
+    const handleSubmit = async (e) => {
+        // empêche le rafraîchissement de la page
+        e.preventDefault();
+
+        // Envoi une requêtre en base de donées pour enregistrer la tâche
+        try{
+            console.log(login);
+            const response = await axios.post(URL.ADD_TASK, task);
+                console.log(response);
+          }catch(e){
+            console.log(e);
+          }
     }
 
   return (
     <main className='app__dashboard'>
-        <form className='dashboard__form'>
+        <form onSubmit={handleSubmit} className='dashboard__form'>
             <div className='form__title'>
-                <label htmlFor="title">Titre</label>
-                <input onChange={handleChange} type="text" name='title'/>
+                <label htmlFor="title">titre</label>
+                <input required onChange={handleChange} type="text" name='title'/>
             </div>
             <div className='form__content'>
-                <label htmlFor="content">Contenu</label>
-                <textarea onChange={handleChange} name="content" cols="30" rows="10"></textarea>
+                <label htmlFor="content">contenu</label>
+                <textarea required onChange={handleChange} name="content" cols="30" rows="10"></textarea>
+            </div>
+            <div className="form__priority">
+                <label htmlFor="number">priorité</label>
+                <input required onChange={handleChange} type="number" name="priority" />
             </div>
             <button>Enregistrer</button>
         </form>
---
+++ frontend/src/Utils/URL.js
@@ -0,0 +1,3 @@
+export const URL = {
+    ADD_TASK : "http://localhost:5678/api/tasks",
+}
