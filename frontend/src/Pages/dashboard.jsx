a96228cc253a9dc1973d3d22bc1ad4f1430cb4e6
Dorsan_Julien <juliendorsan@gmail.com>
2024-04-22 15:06:14 +0200

    Creation du html pour créer une note

--- frontend/src/Pages/dashboard.jsx
+++ frontend/src/Pages/dashboard.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <main className='app__dashboard'>
+        <form className='dashboard__form'>
+            <div className='form__title'>
+                <label htmlFor="title">Titre</label>
+                <input type="text" name='title'/>
+            </div>
+            <div className='form__content'>
+                <label htmlFor="content">Contenu</label>
+                <textarea name="content" cols="30" rows="10"></textarea>
+            </div>
+            <button>Enregistrer</button>
+        </form>
+    </main>
   )
 }
 
