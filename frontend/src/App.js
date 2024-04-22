c34015df719b6e7bda5f899bf2805b57f3b48bc1
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-04-22 15:46:54 +0200

    Mise en place du routage au niveau frontend + intégration des pages Sign et Signup.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Sign from './pages/Sign';
+import SignUp from './pages/SignUp';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/sign' element={<Sign />} />
+      </Routes>
+    </Router>
   );
 }
 
