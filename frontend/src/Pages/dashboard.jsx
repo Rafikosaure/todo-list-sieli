4a2e6423ee197359c3218d8504a2ee5fae388d27
Dorsan_Julien <juliendorsan@gmail.com>
2024-04-22 15:14:39 +0200

    Ajout de la logique pour enregistrer les entrées utilisateur

--- frontend/src/Pages/dashboard.jsx
+++ frontend/src/Pages/dashboard.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Dashboard() {
+
+    // state task pour enregistrer les changements
+    const [task,setTask] = useState({})
+
+
+    const handleChange = (e) => {
+        // récupération du nom et de la valeur de l'évènement
+        const  { name, value} = e.target;
+        // changement du state task
+        setTask((task) => ({...task, [name]:value}));
+        console.log(task); 
+    }
+
   return (
     <main className='app__dashboard'>
         <form className='dashboard__form'>
             <div className='form__title'>
                 <label htmlFor="title">Titre</label>
-                <input type="text" name='title'/>
+                <input onChange={handleChange} type="text" name='title'/>
             </div>
             <div className='form__content'>
                 <label htmlFor="content">Contenu</label>
-                <textarea name="content" cols="30" rows="10"></textarea>
+                <textarea onChange={handleChange} name="content" cols="30" rows="10"></textarea>
             </div>
             <button>Enregistrer</button>
         </form>
